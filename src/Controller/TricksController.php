09fed10d056b17de7e808d223ba2e8f891627e65
JalalEddine <jalaleddine.elhabbazi@gmail.com>
2021-10-13 23:18:47 +0200

    add redirection after submition of a figure

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -9,14 +9,18 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TricksController extends AbstractController
 {
     private $entityManager;
-    public function __construct(EntityManagerInterface $entityManager)
+    private UrlGeneratorInterface $urlGenerator;
+    public function __construct(EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator)
     {
         $this->entityManager = $entityManager;
+        $this->urlGenerator = $urlGenerator;
     }
     /**
      * @Route("/figures/ajouter", name="tricks")
@@ -46,6 +50,7 @@ class TricksController extends AbstractController
                 $this->entityManager->persist($trick);
                 // execute the query with Doctrine
                 $this->entityManager->flush($trick);
+                return new RedirectResponse($this->urlGenerator->generate('account'));
             } else {
                 $errors = "Ce nom est déja utilisé";
             }
