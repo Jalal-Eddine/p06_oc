318eb543cb55b3e2cbba834e3269ddba65e5da91
JalalEddine <jalaleddine.elhabbazi@gmail.com>
2021-10-19 23:50:33 +0200

    add link to delete video

--- public/assets/js/images.js
+++ public/assets/js/images.js
@@ -10,7 +10,7 @@ window.onload = () => {
       e.preventDefault();
 
       // On demande confirmation
-      if (confirm("Voulez-vous supprimer cette image ?")) {
+      if (confirm("Voulez-vous supprimer ?")) {
         // On envoie une requête Ajax vers le href du lien avec la méthode DELETE
         fetch(this.getAttribute("href"), {
           method: "DELETE",
